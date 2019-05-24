7b218b9063c58dbed6893ab9734bc271ed77ddad
ahmedkmadani <ahmedkamal543@gmail.com>
2019-05-24 14:44:20 +0000

    add watson cred

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import urllib3, json, requests
 
 app = Flask(__name__)
 
+#IBM Watson Credenials 
+wml_credentials={
+    "url":'https://eu-gb.ml.cloud.ibm.com',
+    "username": '461336f2-8984-492a-b72a-9376b8e9d1c2',
+    "password": 'de3136e2-2a65-48cd-85f7-77dd03715ba3'
+    }
+
+#init header and request and getting response 
+headers = urllib3.util.make_headers(basic_auth='{username}:{password}'.format(username=wml_credentials['username'], password=wml_credentials['password']))
+url = '{}/v3/identity/token'.format(wml_credentials['url'])
+response = requests.get(url, headers=headers)
+mltoken = json.loads(response.text).get('token')
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
 @app.route('/price', methods=['GET'])
 def getPrice():
